99de6b597a23c6fcd77200d8bbb9419daef7a33b
André Nogueira <andre.nogueira@epfl.ch>
2021-07-16 11:36:28 +0200

    fix: update types

--- plugins/auth/interfaces/auth.d.ts
+++ plugins/auth/interfaces/auth.d.ts
@@ -4,6 +4,11 @@ declare module 'fastify' {
         memberId: string;
     }
     interface FastifyInstance {
+        corsPluginOptions: {
+            origin: [];
+            credentials: boolean;
+            maxAge: number;
+        };
         /**
          * Verify authentication based on session cookie or auth token,
          * extract member from it, and set `request.member`.
